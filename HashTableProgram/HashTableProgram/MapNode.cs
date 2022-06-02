76e7530954b18959dd96425fe372ef96e8121d92
chinki kumari singh <singhchinkikumari.com>
2022-06-02 16:04:29 +0530

    [chinki] Ability to Find Frequency of Words in a Sentence

---
+++ HashTableProgram/HashTableProgram/MapNode.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProgram
+{
+
+    public class MapNode<k, v>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<k, v>>[] items;
+        public MapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<k, v>>[size];
+        }
+        public int GetArrayPosition(k key)
+        {
+            int poistion = key.GetHashCode() % size;
+            return Math.Abs(poistion);
+        }
+        public int Get(k key)
+        {
+            int poistion = GetArrayPosition(key);
+            LinkedList<KeyValue<k, v>> linkedList = GetLinkedList(poistion);
+            foreach (KeyValue<k, v> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    string s = item.Value.ToString();
+                    return int.Parse(s);
+                }
+            }
+            return 0;
+        }
+
+        public LinkedList<KeyValue<k, v>> GetLinkedList(int poistion)
+        {
+            LinkedList<KeyValue<k, v>> linkedList = items[poistion];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<k, v>>();
+                items[poistion] = linkedList;
+            }
+            return linkedList;
+        }
+
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+        public void Add(k key, v value)
+        {
+            int poistion = GetArrayPosition(key);
+            LinkedList<KeyValue<k, v>> linkedList = GetLinkedList(poistion);
+            KeyValue<k, v> item = new KeyValue<k, v>() { Key = key, Value = value, };
+            linkedList.AddFirst(item);
+        }
+        public void Remove(k key)
+        {
+            int poistion = GetArrayPosition(key);
+            LinkedList<KeyValue<k, v>> linkedList = GetLinkedList(poistion);
+            bool itemFound = false;
+            KeyValue<k, v> foundItem = default(KeyValue<k, v>);
+            foreach (KeyValue<k, v> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                Console.WriteLine(key + " removed ");
+                linkedList.Remove(foundItem);
+            }
+
+        }
+
+    }
+}
---
+++ HashTableProgram/HashTableProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using HashTableProgram;
+
+namespace DataStructureUsingHashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MapNode<string, int> myMapNode = new MapNode<string, int>(5);
+            string sentence = "To be or not to be";
+            string[] words = sentence.Split(" ");
+            foreach (string word in words)
+            {
+                int value = myMapNode.Get(word);
+                if (value == 0)
+                    value = 1;
+                else
+                    value = value + 1;
+                myMapNode.Add(word, value);
+            }
+            int frequency = myMapNode.Get("be");
+            Console.WriteLine("Frequency of word in sentence = " + frequency);
+
+            sentence = "Paranoids are not paranoid because they are paranoid but because" +
+            " they keep putting themselves deliberately into paranoid avoidable situations";
+            words = sentence.Split(" ");
+            foreach (string word in words)
+            {
+                int value = myMapNode.Get(word);
+                if (value == 0)
+                    value = 1;
+                else
+                    value = value + 1;
+                myMapNode.Add(word, value);
+            }
+            frequency = myMapNode.Get("paranoid");
+            Console.WriteLine("Frequency of word in paragraph = " + frequency);
+            myMapNode.Remove("avoidable");
+        }
+    }
+}
