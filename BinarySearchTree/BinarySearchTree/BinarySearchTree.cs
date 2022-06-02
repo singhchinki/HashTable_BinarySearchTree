d431d69fd10f2e3716dd6cd10dfb4fdc267090e1
chinki kumari singh <singhchinkikumari.com>
2022-06-02 23:18:03 +0530

    [chinki] Ability to Create Binary Search Tree

---
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        public T NodeData
+        {
+            get;
+            set;
+        }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        int leftCount = 0;
+        int rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new BinarySearchTree<T>(item);
+                else
+                    this.leftTree.Insert(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                    this.rightTree = new BinarySearchTree<T>(item);
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightTree.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using BinarySearchTree;
+using System;
+
+namespace DataStructureUsingBinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+        }
+    }
+}
